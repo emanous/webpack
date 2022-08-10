2f8f378d974731f6fb122fd1fa09334d0105676e
emanous <108159625+emanous@users.noreply.github.com>
2022-08-10 15:17:40 +1000

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,42 @@
-const WorkboxPlugin = require('workbox-webpack-plugin')
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: "./src/client/index.js",
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/client/views/index.html",
-      filename: "./index.html",
-    }),
-    new WorkboxPlugin.GenerateSW(),
-  ],
-  output: {
-    libraryTarget: "var",
-    library: "Client",
-  },
-};
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+
+       
+       { 
+           
+        test:/\/scss$/,
+        use:[MiniCssExtractPlugin.loader, "cssloader" , " sass-loader"]
+
+       },
+       
+    {
+         
+           test: /\.(png|jpe?g|gif)$/i,
+           loader: 'file-loader',
+           options: {
+          publicPath: 'assets',
+        }
+      }
+    ]
+       
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
 
